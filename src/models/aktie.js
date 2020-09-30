8fa4de494210e9d865a58fa2d6945377627e6d18
UsingPython <jonnyschultze@hotmail.de>
2020-09-30 11:07:14 +0200

    Add kurs property to DepotAktien-Object

--- src/models/aktie.js
+++ src/models/aktie.js
@@ -3,10 +3,12 @@ class Aktie {
     /**
      * @param  {number} wkn=null - WKN der Aktie im Depot
      * @param  {number} stueckzahl=null - Anzahl der Aktien im Depot
+     * @param  {number} kurs=null - Kurs der Aktie
      */
-    constructor(wkn=null, stueckzahl=null) {
+    constructor(wkn=null, stueckzahl=null, kurs=null) {
         this.wkn = wkn;
         this.stueckzahl = stueckzahl;
+        this.kurs = kurs;
     }
 
     set wkn(wkn) {
@@ -31,6 +33,18 @@ class Aktie {
      */
     get stueckzahl() {
         return this._stueckzahl;
+    }
+
+    set kurs(kurs) {
+        this._kurs = kurs;
+    }
+
+    /**
+     * @public
+     * @property  {number} kurs - Aktueller Kurs der AG im Depot
+     */
+    get kurs() {
+        return this._kurs
     }
 }
 
