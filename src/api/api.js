9890931136ad4e694b832f7021039d908f59f390
UsingPython <jonnyschultze@hotmail.de>
2020-10-06 13:01:46 +0200

    Split Classes in seperate files

--- src/api/api.js
+++ src/api/api.js
@@ -1,54 +1,5 @@
-const axios = require("axios").default;
 const parse = require("../parser");
-
-/** 
- * Objekt zur Authentifizierung im AG-Spiel 
- */
-class Auth {
-    /**
-     * @param  {string} agspielCookie - Einzigartiger "ag-spiel"-Cookie, der zum Log-in benötigt wird.
-     */
-    constructor(agspielCookie) {
-        this.agsCookie = agspielCookie;
-        this.client = this.agsCookie;
-    }
-
-    set agsCookie(cookie) {
-        this._agsCookie = cookie;
-    }
-    /**
-     * @public
-     * @property {string} agsCookie - Einzigartiger "ag-spiel"-Cookie, der zum Log-in benötigt wird.
-     */
-    get agsCookie() {
-        return this._agsCookie;
-    }
-    
-    /**
-     * Erstellt Axios-Client mit dem AG-Spiel-Cookie - wird für die Requests benötigt.
-     * @private
-     * @param {string} - AGS-Cookie
-     */
-    set client(cookie) {
-        this._client = axios.create({
-            baseURL: "https://www.ag-spiel.de/",
-            headers: {
-                "Cookie": `ag-spiel=${cookie};`
-            }
-        });
-        this._client.interceptors.response.use((response) => {
-            if (typeof response.data == "string" && response.data.includes("ssen sich registrieren")) {
-                throw new Error("Du musst dich einloggen! Kein valider Cookie wurde hinterlegt.");
-            } else {
-                return response;
-            }
-        });
-    }
-
-    get client() {
-        return this._client;
-    }
-}
+const Auth = require("./auth");
 
 /** Objekt zum Abfragen der Daten aus der offiziellen AG-Spiel API und den Webseiten. Wird durch das "Auth"-Objekt erweitert.
  * @extends Auth
